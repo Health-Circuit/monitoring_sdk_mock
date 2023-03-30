35bceee50b56da65bd70e2b8b045ec7eda4b7e26
Juategi <lookinmeal@gmail.com>
2023-03-30 11:11:51 +0200

    commit prueba

--- lib/monitoring_mock.dart
+++ lib/monitoring_mock.dart
@@ -1,4 +1,3 @@
-
 import 'package:hive_flutter/adapters.dart';
 import 'package:monitoring_mock/paired_devices.dart';
 import 'package:uuid/uuid.dart';
@@ -6,10 +5,10 @@ import 'package:uuid/uuid.dart';
 import 'monitoring_mock_platform_interface.dart';
 
 class MonitoringMock {
-
   Future<void> initPlugin() async {
     await Hive.initFlutter();
     await Hive.openBox("pairedDevicesBox");
+
     Hive.registerAdapter(PairedDeviceAdapter());
   }
 
@@ -22,7 +21,8 @@ class MonitoringMock {
     final items = box.values;
     final List<PairedDevice> pairedDevices = items.toList().map((dynamic item) {
       final List<dynamic> fields = item as List<dynamic>;
-      return PairedDevice.m(fields[0] as Uuid, fields[1] as String, fields[2] as String);
+      return PairedDevice.m(
+          fields[0] as Uuid, fields[1] as String, fields[2] as String);
     }).toList();
     return pairedDevices;
   }
